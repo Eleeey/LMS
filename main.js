95f124fc5ceffe1ca6c93193cf98e1daf1cd88df
Eleeey <ezralaakasen@gmail.com>
2023-04-24 06:57:12 -0700

    fedite project

--- main.js
+++ main.js
@@ -1,28 +1,6 @@
-console.log('Hello World!');
-// Get the slider element
-const slider = document.querySelector('.slider');
-
-// Get the slides
-const slides = slider.querySelectorAll('.slide');
-
-// Set the initial slide index
-let currentSlide = 0;
-
-// Set the slide width
-const slideWidth = slides[currentSlide].clientWidth;
-
-// Set the slides container width
-const slidesContainerWidth = slideWidth += slides.length;
-
-// Set the slides container styles
-slider.querySelector('.slides').style.width = `${slidesContainerWidth}px`;
-
-// Add event listener for the next button
-slider.querySelector('.next').addEventListener('click', () => {
-  // Move to the next slide
-  currentSlide++;
-
-  // If we've reached the end, go back to the beginning
-  if (currentSlide==slides.length) {
-   currentSlide==0;
-}})
+var toggleBtn = document.querySelector(".toggle")
+var drpMenu = document.querySelector(".links")
+console.log(drpMenu)
+toggleBtn.addEventListener('click',()=>{
+  drpMenu.classList.toggle("drop");
+})
